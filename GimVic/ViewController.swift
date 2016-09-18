cdf42224df1b553be7144967a2f44dcb5563ef14
DzinVision <vid.drobnic@gmail.com>
2016-09-18 20:21:51 +0200

    Created Xcode Project

---
+++ GimVic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GimVic
+//
+//  Created by Vid Drobnič on 9/18/16.
+//  Copyright © 2016 Vid Drobnič. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
